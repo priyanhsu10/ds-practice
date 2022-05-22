730c3181cf415631ac3938c5c88fa001d66a9093
priyanshu10 <priyanshuparate@gmail.com>
2022-05-22 22:49:16 +0530

    graph traverals

---
+++ tree/Solution.java
@@ -0,0 +1,50 @@
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        boolean r = isPalindrome(s);
+    }
+
+    public static boolean isPalindrome(String s) {
+
+        // make string to lovercase remove unnenesory caractor
+
+        return isPal(filterString(s));
+
+    }
+
+    static char[] filterString(String s) {
+        char[] chars = new char[s.length()];
+        int currentindex = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int num = (int) s.charAt(i);
+            if (num >= 65 && num <= 90) {
+                chars[currentindex] = (char) (num + 32);
+                currentindex++;
+            } else if (num >= 97 && num <= 122) {
+                chars[currentindex] = (char) num;
+                currentindex++;
+            }
+
+        }
+        return chars;
+    }
+
+    static boolean isPal(char[] s) {
+        int len = s.length;
+        int backword = s.length - 1;
+
+        for (int i = 0; i < s.length; i++) {
+
+            if (i == s.length && backword == 0) {
+                return true;
+            }
+            if (s[i] == s[backword]) {
+                return false;
+            }
+            backword--;
+
+        }
+        return false;
+    }
+}
