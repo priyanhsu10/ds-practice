6013e44aaec994bafb8ca1f9dc321b890b2319be
priyanshu10 <priyanshuparate@gmail.com>
2022-06-06 10:23:36 +0530

    best sum dynamic problem

--- tree/dynamic/howsum.py
+++ tree/dynamic/howsum.py
@@ -24,10 +24,38 @@ def howsum(arry, target: int, memo):
     return None
 
 
+def bestsum(arry, target: int, memo):
+    if memo == None:
+        memo = {}
+    if target in memo:
+        return memo[target]
+
+    if target == 0:
+        return []
+    if target < 0:
+        return None
+
+    min = None
+    minLen = 100000_000
+    for x in arry:
+        rem = target-x
+        r = bestsum(arry, rem, memo)
+        if r != None:
+            memo[target] = [x, *r]
+            if len(r) < minLen:
+                min = memo[target]
+                minLen = len(r)
+
+    if minLen == 100000_000:
+        return None
+    else:
+        return min
+
+
 print(__name__)
 if __name__ == "__main__":
     # print(howsum([2, 3], 7))
-    print(howsum([5, 3, 4, 7], 7, {}))
-    print(howsum([2, 4], 7, {}))
-    print(howsum([2, 3, 5], 8, {}))
+    print(bestsum([5, 3, 4, 7], 7, {}))
+    print(bestsum([2, 4], 7, {}))
+    print(bestsum([2, 3, 5], 8, {}))
     print(howsum([7, 14], 300, {}))
