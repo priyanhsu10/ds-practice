0f5ec17d5b5fd4bc7340f83d876cdc25480ad0f5
priyanshu10 <priyanshuparate@gmail.com>
2022-11-21 10:55:38 +0530

    pattern questions

--- cpp/patterns.cpp
+++ cpp/patterns.cpp
@@ -213,6 +213,34 @@ void patern8(int n)
         cout << endl;
     }
 }
+// 0-1 patern
+// n=5
+// 1
+// 0 1
+// 1 0 1
+// 0 1 0 1
+// 1 0 1 0 1
+void patern9(int n)
+{
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j <= i; j++)
+        {
+            if ((i + j) % 2 == 0)
+            {
+                cout << "1 ";
+            }
+            else
+            {
+
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
@@ -228,5 +256,6 @@ int main()
     // patern5(n);
     // patern6(n);
     // patern7(n);
-    patern8(n);
+    // patern8(n);
+    patern9(n);
 }
